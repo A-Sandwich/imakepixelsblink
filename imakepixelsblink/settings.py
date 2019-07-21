91ab85af2c3968ce1c1f13a365c469a9af015168
Kyle Burkholder <kbburkholder@gmail.com>
2019-07-21 12:21:26 -0400

    Trying to get around scoping issue.

--- imakepixelsblink/settings.py
+++ imakepixelsblink/settings.py
@@ -73,7 +73,7 @@ WSGI_APPLICATION = 'imakepixelsblink.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
+DATABASES=''
 if os.environ.get('ENVIRONMENT', 'DEVELOPMENT') == 'PRODUCTION':
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
